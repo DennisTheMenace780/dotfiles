60f9d238e907ef10e0361d15efbc79d910d954a6
Dennis Gray <dennis.gray@shopify.com>
2022-10-30 11:30:54 -0700

    initial commit

---
+++ .config/nvim/init.lua
@@ -0,0 +1,10 @@
+
+-- Load Options, Keymaps, and Plugins
+require "user.options"
+require "user.keymaps"
+require "user.plugins"
+require "user.cmp"
+require "user.lsp"
+require "user.telescope"
+
+vim.g.netrw_liststyle = 3 -- probably want to move this
---
+++ .config/nvim/lua/user/plugins.lua
@@ -0,0 +1,89 @@
+local fn = vim.fn
+
+-- Automatically install packer
+local install_path = fn.stdpath "data" .. "/site/pack/packer/start/packer.nvim"
+if fn.empty(fn.glob(install_path)) > 0 then
+  PACKER_BOOTSTRAP = fn.system {
+    "git",
+    "clone",
+    "--depth",
+    "1",
+    "https://github.com/wbthomason/packer.nvim",
+    install_path,
+  }
+  print "Installing packer close and reopen Neovim..."
+  vim.cmd [[packadd packer.nvim]]
+end
+
+-- Autocommand that reloads neovim whenever you save the plugins.lua file
+vim.cmd [[
+  augroup packer_user_config
+    autocmd!
+    autocmd BufWritePost plugins.lua source <afile> | PackerSync
+  augroup end
+]]
+
+-- Use a protected call so we don't error out on first use
+local status_ok, packer = pcall(require, "packer")
+if not status_ok then
+  return
+end
+
+-- Have packer use a popup window
+packer.init {
+  display = {
+    open_fn = function()
+      return require("packer.util").float { border = "rounded" }
+    end,
+  },
+}
+
+-- Install your plugins here
+return packer.startup(function(use)
+  -- My plugins here
+  use "wbthomason/packer.nvim" -- Have packer manage itself
+  use "nvim-lua/popup.nvim" -- An implementation of the Popup API from vim in Neovim
+  use "nvim-lua/plenary.nvim" -- Useful lua functions used ny lots of plugins
+
+  -- Vim Markdown
+--   use "preservim/vim-markdown"
+  -- Colour Themes
+  use {
+	"catppuccin/nvim",
+	as = "catppuccin",
+	config = function()
+		vim.g.catppuccin_flavour = "macchiato" -- latte, frappe, macchiato, mocha
+		require("catppuccin").setup()
+		vim.api.nvim_command "colorscheme catppuccin"
+	end
+}
+
+  -- Icon Themes
+  use "ryanoasis/vim-devicons"
+
+  -- CMP Plugins
+  use "hrsh7th/nvim-cmp" -- Completion Plugin
+  use "hrsh7th/cmp-buffer" -- Buffer Completions
+  use "hrsh7th/cmp-path"  -- Path Completions
+  use "hrsh7th/cmp-cmdline" -- CMD Line completions
+  use "hrsh7th/cmp-nvim-lsp" -- LSP completions
+  use "hrsh7th/cmp-nvim-lua"
+
+
+  -- LSP Packages
+  use "neovim/nvim-lspconfig" -- enable LSP
+  use "williamboman/nvim-lsp-installer" -- Easy installer for language servers
+
+    -- snippets
+  use "L3MON4D3/LuaSnip" --snippet engine
+  use "rafamadriz/friendly-snippets" -- a bunch of snippets to use
+  -- Telescope
+  use "nvim-telescope/telescope.nvim"
+  use {'nvim-telescope/telescope-fzf-native.nvim', run = 'make' }
+
+  -- Automatically set up your configuration after cloning packer.nvim
+  -- Put this at the end after all plugins
+  if PACKER_BOOTSTRAP then
+    require("packer").sync()
+  end
+end)
