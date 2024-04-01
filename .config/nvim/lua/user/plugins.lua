c498b58b808ca5deb49d4e85cfa339b98452014a
Dennis Gray <dennis.g@getjobber.com>
2024-03-31 19:47:25 -0700

    add everforest for notes

--- .config/nvim/lua/user/plugins.lua
+++ .config/nvim/lua/user/plugins.lua
@@ -45,7 +45,15 @@ return packer.startup(function(use)
 	use("nvim-lua/popup.nvim") -- An implementation of the Popup API from vim in Neovim
 	use("nvim-lua/plenary.nvim") -- Useful lua functions used ny lots of plugins
 
-	use("prettier/vim-prettier")
+	-- use("prettier/vim-prettier")
+
+	use({
+		"neanias/everforest-nvim",
+		-- Optional; default configuration will be used if setup isn't called.
+		config = function()
+			require("everforest").setup()
+		end,
+	})
 
 	use({ "windwp/nvim-ts-autotag" })
 	-- DapUI
